80f2b1904340fa21661af91da029b889e38fc793
Pavel Akashev <pavela29@gmail.com>
2025-04-18 20:33:11 +0300

    #3 CatsViewModel connected to MainActivity

--- app/src/main/java/otus/homework/coroutines/CatsViewModel.kt
+++ app/src/main/java/otus/homework/coroutines/CatsViewModel.kt
@@ -2,9 +2,17 @@ package otus.homework.coroutines
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.launch
 
 class CatsViewModel(
     private val catsService: CatsService,
@@ -15,7 +23,28 @@ class CatsViewModel(
         CrashMonitor.trackWarning(throwable)
     }
 
+    private val _uiState = MutableStateFlow<CatsUIState?>(null)
+    val uiState = _uiState.onStart { load() }
+        .filterNotNull()
+        .shareIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            replay = 1
+        )
 
+    fun load() {
+        viewModelScope.launch(exceptionHandler) {
+            val fact = async { catsService.getCatFact() }
+            val image = async { imageService.getCatImage() }
+
+            _uiState.emit(
+                CatsUIState(
+                    fact = fact.await(),
+                    image = image.await()[0]
+                )
+            )
+        }
+    }
 
     companion object {
         fun provideFactory(
--- app/src/main/java/otus/homework/coroutines/MainActivity.kt
+++ app/src/main/java/otus/homework/coroutines/MainActivity.kt
@@ -1,45 +1,53 @@
 package otus.homework.coroutines
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var catsPresenter: CatsPresenter
-
     private val diContainer = DiContainer()
+
+    private lateinit var fact: TextView
+    private lateinit var image: ImageView
+    private lateinit var loadButton: Button
 
     private val viewModel by lazy {
         ViewModelProvider(
-            this,
-            CatsViewModel.provideFactory(diContainer.catsService, diContainer.imageService)
+            this, CatsViewModel.provideFactory(diContainer.catsService, diContainer.imageService)
         )[CatsViewModel::class.java]
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
+        val view = layoutInflater.inflate(R.layout.activity_main, null)
         setContentView(view)
+        fact = view.findViewById(R.id.fact_textView)
+        image = view.findViewById(R.id.catImage)
+        loadButton = view.findViewById(R.id.button)
 
-        catsPresenter = CatsPresenter(
-            diContainer.catsService,
-            diContainer.imageService,
-            diContainer.presenterScope
-        )
-        view.presenter = catsPresenter
-        catsPresenter.attachView(view)
-        catsPresenter.onInitComplete()
+        configureFact()
+        setListeners()
     }
 
-    override fun onStop() {
-        if (isFinishing) {
-            catsPresenter.apply {
-                detachView()
+    private fun setListeners() {
+        loadButton.setOnClickListener {
+            viewModel.load()
+        }
+    }
+
+    private fun configureFact() {
+        lifecycleScope.launch {
+            viewModel.uiState.collect { state ->
+                fact.text = state.fact.fact
+                Picasso.get().load(state.image.url).into(image)
             }
         }
-        catsPresenter.cancelCoroutine()
-        super.onStop()
     }
 }
